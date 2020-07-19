44b677324def211fd751a42008977b7b04308f3f
Francesco <med.dama@gmail.com>
2020-07-19 20:06:16 +0100

    Add constructor to Sales class

--- src/Sales.js
+++ src/Sales.js
@@ -1,8 +1,18 @@
 import React, {Component} from 'react';
 
 class Sales extends Component {
+    constructor(props) {
+        super(props);
+
+
+
+    }
+
+
     render() {
+        console.log(this.props.items);
         return (
+
             <div>
                 I am from course sales
             </div>
